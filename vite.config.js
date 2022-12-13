9283c84b7c2f64046a5c670394126b0a93d0b440
entriphy <1747088+entriphy@users.noreply.github.com>
2022-12-12 19:06:42 -0800

    Add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,29 @@ import sveltePreprocess from 'svelte-preprocess';
 export default defineConfig({
   plugins: [svelte({
     preprocess: [sveltePreprocess({})]
-  })]
+  }), externalCSSPlugin()]
 })
+
+function externalCSSPlugin() {
+  return {
+    name: 'external-css',
+    transformIndexHtml: {
+      enforce: 'post',
+      transform(html, ctx) {
+        console.log(ctx.server);
+        return [
+          {
+            tag: "link",
+            attrs: {"rel": "stylesheet", "type": "text/css", "href": "/smui.css", "media": "(prefers-color-scheme: light)"},
+            injectTo: ctx.server ? "body.prepend" : "head"
+          },
+          {
+            tag: "link",
+            attrs: {"rel": "stylesheet", "type": "text/css", "href": "/smui-dark.css", "media": "(prefers-color-scheme: dark)"},
+            injectTo: ctx.server ? "body.prepend" : "head"
+          },
+        ]
+      }
+    }
+  }
+}
